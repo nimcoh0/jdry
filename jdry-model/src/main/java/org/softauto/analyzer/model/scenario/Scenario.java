8bf516fc463aa4d912fecee48ced39cba7d14275
nimcoh <nimcoh0@gmail.com>
2023-11-13 18:33:39 +0200

    no message

---
+++ jdry-model/src/main/java/org/softauto/analyzer/model/scenario/Scenario.java
@@ -0,0 +1,158 @@
+package org.softauto.analyzer.model.scenario;
+
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Scenario implements Cloneable, Serializable {
+
+    /**
+     * list of tests names that include in this scenario
+     */
+    LinkedList<String> tests = new LinkedList<>();
+
+    Integer order;
+
+    /**
+     * scenario UUID
+     */
+    String id;
+
+    String namespace;
+
+    /**
+     * scenario name
+     */
+    String suiteName;
+
+    /**
+     * scenario state (can be run,pass,fail,skip)
+     */
+    String state;
+
+    /**
+     * list of errors
+     */
+    List<Object> error = new ArrayList<>();
+
+    /**
+     * scenario configuration
+     */
+    HashMap<String,Object> configuration = new HashMap<>();
+
+    HashMap<String,Object> properties = new HashMap<>();
+
+    public HashMap<String, Object> getProperties() {
+        return properties;
+    }
+
+
+    public List<Object> getError() {
+        return error;
+    }
+
+    public void setError(List<Object> error) {
+        this.error = error;
+    }
+
+    public void addError(Object error) {
+        this.error.add(error);
+    }
+
+    public Object getProperty(String key) {
+        if(properties.containsKey(key)){
+           return properties.get(key);
+        }
+        return null;
+    }
+
+
+    public HashMap<String, Object> getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(HashMap<String, Object> configuration) {
+        this.configuration = configuration;
+    }
+
+    public void setProperties(HashMap<String, Object> properties) {
+        this.properties = properties;
+    }
+
+    public void addProperty(String key,Object value) {
+        this.properties.put(key,value);
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public LinkedList<String> getTests() {
+        return tests;
+    }
+
+    public Scenario setTests(LinkedList<String> tests) {
+        this.tests = tests;
+        return this;
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public Scenario setOrder(Integer order) {
+        this.order = order;
+        return this;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Scenario setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public Scenario setNamespace(String namespace) {
+        this.namespace = namespace;
+        return this;
+    }
+
+    public String getSuiteName() {
+        return suiteName;
+    }
+
+    public Scenario setSuiteName(String suiteName) {
+        this.suiteName = suiteName;
+        return this;
+    }
+
+    public Scenario addTest(String fullName) {
+        this.tests.add(fullName);
+        return this;
+    }
+
+
+
+    public boolean isExist(String name){
+        for(String s : tests){
+            if(s.equals(name)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ jdry-model/src/main/java/org/softauto/analyzer/model/scenario/ScenarioBuilder.java
@@ -0,0 +1,95 @@
+package org.softauto.analyzer.model.scenario;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+import java.util.LinkedList;
+
+public class ScenarioBuilder {
+
+    private static Logger logger = LogManager.getLogger(ScenarioBuilder.class);
+
+    public static Builder newBuilder() { return new Builder();}
+
+    private Scenario scenario;
+
+    public ScenarioBuilder(Scenario scenario){
+        this.scenario = scenario;
+    }
+
+    public Scenario getScenario() {
+        return scenario;
+    }
+
+    public static class Builder {
+
+        private LinkedList<String> tests = new LinkedList<>();
+
+        private Integer order;
+
+        private String id;
+
+        String namespace;
+
+        String suiteName;
+
+       public Builder setSuiteName(String suiteName) {
+            this.suiteName = suiteName;
+            return this;
+        }
+
+        public Builder setNamespace(String namespace) {
+            this.namespace = namespace;
+            return this;
+        }
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+
+
+        public String getId() {
+            return id;
+        }
+
+        public Builder setOrder(Integer order) {
+            this.order = order;
+            return this;
+        }
+
+        public Builder setTests(LinkedList<String> tests) {
+            this.tests = tests;
+            return this;
+        }
+
+
+
+        public Builder addTest(String name) {
+            this.tests.add(name);
+            return this;
+        }
+
+        public LinkedList<String> getTests() {
+            return tests;
+        }
+
+        public ScenarioBuilder build(){
+            Scenario scenario = new Scenario();
+            try {
+                scenario.setTests(tests);
+                scenario.setOrder(order);
+                scenario.setId(id);
+                scenario.setSuiteName(suiteName);
+
+            } catch (Exception e) {
+                logger.error("fail build Scenario "+ id,e);
+            }
+            return new ScenarioBuilder(scenario);
+        }
+
+    }
+}
